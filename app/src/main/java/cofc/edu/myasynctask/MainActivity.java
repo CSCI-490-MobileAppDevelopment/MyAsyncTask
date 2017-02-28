5d10d00f15bfd2ddb906ada78c1aba7d1e81d6cf
Keith Briggs <kbriggs@localhost.fedora24>
2017-02-27 22:58:48 -0500

    Handler

--- app/src/main/java/cofc/edu/myasynctask/MainActivity.java
+++ app/src/main/java/cofc/edu/myasynctask/MainActivity.java
@@ -2,12 +2,44 @@ package cofc.edu.myasynctask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progress;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progress = (ProgressBar) findViewById(R.id.progressBar1);
+        text = (TextView) findViewById(R.id.textView1);
+
     }
+
+    public void startProgress(View view) {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 10; i++) {
+                    final int value = i;
+                    new MyTask().longTask();
+                    progress.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            text.setText("Updating");
+                            progress.setProgress(value);
+                        }
+                    });
+                }
+            }
+        };
+        new Thread(runnable).start();
+    }
+
 }
--- app/src/main/java/cofc/edu/myasynctask/MyTask.java
+++ app/src/main/java/cofc/edu/myasynctask/MyTask.java
@@ -6,5 +6,13 @@ import android.os.AsyncTask;
  * Created by kbriggs on 2/27/17.
  */
 
-public class MyTask extends AsyncTask<> {
+public class MyTask {
+
+    public void longTask () {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
