0e25c178afa61e7c3f6476f9af64e759b8ddbf76
Keith Briggs <kbriggs@localhost.fedora24>
2017-02-27 23:36:19 -0500

    initial async demo

--- app/src/main/java/cofc/edu/myasynctask/MainActivity.java
+++ app/src/main/java/cofc/edu/myasynctask/MainActivity.java
@@ -2,12 +2,44 @@ package cofc.edu.myasynctask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long PASSWORD = 12345;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStartTask(View v) {
+        final long RANGE = 10000000;
+
+        long guess = 0;
+        Random rand = new Random();
+
+        while (guess != PASSWORD) {
+            guess = Math.abs(rand.nextLong() % RANGE);
+        }
+        displayProgress("Done!!");
+        displayAnswer(guess);
+
+    }
+
+    private void displayProgress(String progress) {
+        TextView textView = (TextView) findViewById(R.id.status);
+        textView.setText(progress);
+    }
+
+    private void displayAnswer(long ans) {
+        String message = "The password is " + ans;
+
+        TextView textView = (TextView) findViewById(R.id.answer);
+        textView.setText(message);
     }
 }
--- app/src/main/java/cofc/edu/myasynctask/MyTask.java
+++ app/src/main/java/cofc/edu/myasynctask/MyTask.java
@@ -6,5 +6,24 @@ import android.os.AsyncTask;
  * Created by kbriggs on 2/27/17.
  */
 
-public class MyTask extends AsyncTask<> {
+public class MyTask extends AsyncTask<String, String, String> {
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
 }
